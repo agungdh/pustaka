52705a815fbb3f13d8203a995433ca6c95b87f8e
AgungDH <agungdh@live.com>
2019-03-01 22:11:58 +0700

    update

--- src/Pustaka.php
+++ src/Pustaka.php
@@ -152,36 +152,40 @@ class Pustaka {
 		return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0] . ' ' . $waktu;
 	}
 
-	public static function terbilang($nilai) {
+	public static function penyebut($nilai) {
 		$nilai = abs($nilai);
 		$huruf = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
 		$temp = "";
 		if ($nilai < 12) {
 			$temp = " ". $huruf[$nilai];
 		} else if ($nilai <20) {
-			$temp = self::terbilang($nilai - 10). " belas";
+			$temp = self::penyebut($nilai - 10). " belas";
 		} else if ($nilai < 100) {
-			$temp = self::terbilang($nilai/10)." puluh". self::terbilang($nilai % 10);
+			$temp = self::penyebut($nilai/10)." puluh". self::penyebut($nilai % 10);
 		} else if ($nilai < 200) {
-			$temp = " seratus" . self::terbilang($nilai - 100);
+			$temp = " seratus" . self::penyebut($nilai - 100);
 		} else if ($nilai < 1000) {
-			$temp = self::terbilang($nilai/100) . " ratus" . self::terbilang($nilai % 100);
+			$temp = self::penyebut($nilai/100) . " ratus" . self::penyebut($nilai % 100);
 		} else if ($nilai < 2000) {
-			$temp = " seribu" . self::terbilang($nilai - 1000);
+			$temp = " seribu" . self::penyebut($nilai - 1000);
 		} else if ($nilai < 1000000) {
-			$temp = self::terbilang($nilai/1000) . " ribu" . self::terbilang($nilai % 1000);
+			$temp = self::penyebut($nilai/1000) . " ribu" . self::penyebut($nilai % 1000);
 		} else if ($nilai < 1000000000) {
-			$temp = self::terbilang($nilai/1000000) . " juta" . self::terbilang($nilai % 1000000);
+			$temp = self::penyebut($nilai/1000000) . " juta" . self::penyebut($nilai % 1000000);
 		} else if ($nilai < 1000000000000) {
-			$temp = self::terbilang($nilai/1000000000) . " milyar" . self::terbilang(fmod($nilai,1000000000));
+			$temp = self::penyebut($nilai/1000000000) . " milyar" . self::penyebut(fmod($nilai,1000000000));
 		} else if ($nilai < 1000000000000000) {
-			$temp = self::terbilang($nilai/1000000000000) . " trilyun" . self::terbilang(fmod($nilai,1000000000000));
+			$temp = self::penyebut($nilai/1000000000000) . " trilyun" . self::penyebut(fmod($nilai,1000000000000));
 		}     
-
+		return $temp;
+	}
+ 
+	public static function terbilang($nilai) {
 		if($nilai<0) {
-			return "minus ". ($temp);
+			$hasil = "minus ". trim(self::penyebut($nilai));
 		} else {
-			return ($temp);
-		}  
-	} 
+			$hasil = trim(self::penyebut($nilai));
+		}     		
+		return $hasil;
+	}
 }
