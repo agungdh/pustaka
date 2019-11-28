2eba803923a66e17f4aa47695410e4ef5c48211b
AgungDH <agungdh@live.com>
2019-11-28 18:57:41 +0700

    .

--- src/Pustaka.php
+++ src/Pustaka.php
@@ -269,4 +269,50 @@ class Pustaka {
 		}     		
 		return $hasil;
 	}
+
+	public static function caesarencrypt($str, $offset) {
+	    $encrypted_text = "";
+	    $offset = $offset % 26;
+	    if($offset < 0) {
+	        $offset += 26;
+	    }
+	    $i = 0;
+	    while($i < strlen($str)) {
+	        $c = strtoupper($str{$i}); 
+	        if(($c >= "A") && ($c <= 'Z')) {
+	            if((ord($c) + $offset) > ord("Z")) {
+	                $encrypted_text .= chr(ord($c) + $offset - 26);
+	        } else {
+	            $encrypted_text .= chr(ord($c) + $offset);
+	        }
+	      } else {
+	          $encrypted_text .= " ";
+	      }
+	      $i++;
+	    }
+	    return $encrypted_text;
+	}
+
+	public static function caesardecrypt($str, $offset) {
+	    $decrypted_text = "";
+	    $offset = $offset % 26;
+	    if($offset < 0) {
+	        $offset += 26;
+	    }
+	    $i = 0;
+	    while($i < strlen($str)) {
+	        $c = strtoupper($str{$i}); 
+	        if(($c >= "A") && ($c <= 'Z')) {
+	            if((ord($c) - $offset) < ord("A")) {
+	                $decrypted_text .= chr(ord($c) - $offset + 26);
+	        } else {
+	            $decrypted_text .= chr(ord($c) - $offset);
+	        }
+	      } else {
+	          $decrypted_text .= " ";
+	      }
+	      $i++;
+	    }
+	    return $decrypted_text;
+	}
 }
